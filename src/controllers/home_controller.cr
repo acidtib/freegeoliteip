1d17a89d5ed979a8d06c03df4de99d0a6d818644
Dainel Vera <arubinofaux@gmail.com>
2021-01-23 01:53:18 -0700

    home route returns self ip

--- src/controllers/home_controller.cr
+++ src/controllers/home_controller.cr
@@ -2,15 +2,13 @@ module FREEGEOLITEIP
   module Controllers
     class HomeController < Grip::Controllers::Http
       def index(context : Context)
-        puts context.request.headers.get?("X-Forwarded-For")
-        puts context.request.inspect
-        puts context.request.remote_address
+        address = context.request.headers.get?("X-Forwarded-For").first
 
         context
           .put_status(200)
           .json(
             {
-              "id" => 1
+              "ip" => address
             }
           )
       end
