a63b9897be495697fdedfcefc1aa2820176d43a7
Dainel Vera <arubinofaux@gmail.com>
2021-01-23 04:16:07 -0700

    hu

--- src/controllers/home_controller.cr
+++ src/controllers/home_controller.cr
@@ -2,7 +2,7 @@ module FREEGEOLITEIP
   module Controllers
     class HomeController < Grip::Controllers::Http
       def index(context : Context)
-        address = context.request.headers.get("X-Forwarded-For").to_s.split(",")[0].gsub("[\/", "")
+        address = context.request.headers.get("X-Forwarded-For").to_s.split(",")[0].gsub("[\\", "")
 
         context
           .put_status(200)
