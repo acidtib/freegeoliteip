b52c22d18de9a58abd21262d990c983fce410dff
Dainel Vera <arubinofaux@gmail.com>
2021-01-30 21:48:47 -0700

    wip

--- src/jobs/ipv4/sync_ipv4_geoip_city.cr
+++ src/jobs/ipv4/sync_ipv4_geoip_city.cr
@@ -1,16 +1,16 @@
-class FREEGEOLITEIP::SyncIpv4GeoIpJob < Mosquito::PeriodicJob
+class FREEGEOLITEIP::Jobs::SyncIpv4GeoIpCity < Mosquito::PeriodicJob
   run_every 7200.minutes
   
   def perform
     db_data = IO::Memory.new
 
-    HTTP::Client.get("https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-ASN&license_key=#{ENV["MAXMIND_LICENSE_KEY"]}&suffix=tar.gz") do |response|
+    HTTP::Client.get("https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=#{ENV["MAXMIND_LICENSE_KEY"]}&suffix=tar.gz") do |response|
       pp response.status_code 
       if response.status_code == 200
         Compress::Gzip::Reader.open(response.body_io) do |gzip|
           Crystar::Reader.open(gzip) do |tar|
             tar.each_entry do |entry|
-              if entry.name.includes?("GeoLite2-ASN.mmdb")
+              if entry.name.includes?("GeoLite2-City.mmdb")
                 pp entry.name
                 IO.copy entry.io, db_data
                 break
@@ -23,7 +23,7 @@ class FREEGEOLITEIP::SyncIpv4GeoIpJob < Mosquito::PeriodicJob
       end
     end
 
-    asn_data = MaxMindDB.open(db_data)
+    city_data = MaxMindDB.open(db_data)
 
     # generate range of all possible ipv4 addresses
     if ENV["APP_ENV"] == "development"
@@ -36,8 +36,8 @@ class FREEGEOLITEIP::SyncIpv4GeoIpJob < Mosquito::PeriodicJob
 
     ranges.each do |r|
       begin
-        raw_data = asn_data.get(r.address)
-        ReadIpv4AsnJob.new(raw_data, r.address).perform
+        raw_data = city_data.get(r.address)
+        # ReadIpv4AsnJob.new(raw_data, r.address).perform
       rescue exception
         pp exception
       end
